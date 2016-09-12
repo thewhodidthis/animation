825d79688c97488d84172397f93f90d7e1508618
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2016-09-12 20:57:33 +0200

    example from scratch

--- example/index.js
+++ example/index.js
@@ -1,52 +1,7 @@
-(function(window, document, undefined) {
-  'use strict';
+'use strict';
 
-  var Animation = window.animation;
+var animation = new Animation(function _onEachFrame() {
+  // Do something
+});
 
-  // TODO: Add up down classes
-
-  var edge = 40;
-  var scrollY = 0;
-
-  var body = document.body;
-  var html = document.documentElement;
-  var wrap = document.querySelector('.wrap');
-  var ball = wrap.cloneNode(false);
-
-  var loop = new Animation(function _onTick(now) {
-    var cutoff = window.innerHeight * 0.25;
-
-    if (scrollY <= edge || scrollY >= window.innerHeight - edge) {
-      ball.classList.add('shrink');
-    } else {
-      ball.classList.remove('shrink');
-    }
-
-    if (scrollY >= cutoff) {
-      ball.classList.add('trip');
-    } else {
-      ball.classList.remove('trip');
-    }
-
-    if (scrollY >= cutoff * 3) {
-      ball.classList.add('roll');
-    } else {
-      ball.classList.remove('roll');
-    }
-
-    loop.stop();
-  });
-
-  wrap.appendChild(ball);
-  ball.className = 'ball shrink';
-
-  window.addEventListener('load', function _onLoad(e) {
-    html.className = 'html';
-  });
-
-  window.addEventListener('scroll', function _onScroll(e) {
-    scrollY = window.scrollY;
-
-    loop.start();
-  }, false);
-})(window, document);
+document.documentElement.className = 'html';
