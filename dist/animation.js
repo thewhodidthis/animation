9815002cb771c19edf9d67a61dca9b97f84d9a2f
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-03-17 15:59:25 +0200

    Rebuild

--- dist/animation.js
+++ dist/animation.js
@@ -1,37 +1,55 @@
 var Animation = (function () {
   'use strict';
 
+  // # Animation
+  // rAF loop toggle
+
   var Animation = function Animation(callback) {
+    if (callback === undefined || typeof callback !== 'function') {
+      throw Error('Missing valid callback');
+    }
+
+    // Is running flag
     var frameId = null;
 
+    // Frame
     var tick = function tick(fn) {
+      // Mark frame
       frameId = window.requestAnimationFrame(fn);
     };
 
+    // On each frame
     var loop = function loop(now) {
       callback(now);
 
-      // Skip when turned off
+      // Pause if idle
       if (frameId) {
         tick(loop);
       }
     };
 
+    // Turn off
+    var stop = function stop() {
+      // Make sure it's running
+      if (frameId) {
+        // Falsify frame
+        frameId = window.cancelAnimationFrame(frameId);
+      }
+    };
+
+    // Kick off
     var start = function start() {
       // Make sure these don't stack up
       if (!frameId) {
         tick(loop);
       }
-    };
 
-    var stop = function stop() {
-      if (frameId) {
-        // Turn off, falsify frameId
-        frameId = window.cancelAnimationFrame(frameId);
-      }
+      return frameId;
     };
 
     return {
+      play: start,
+      pause: stop,
       start: start,
       stop: stop
     };
