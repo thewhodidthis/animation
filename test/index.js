6a1b06943dfa1cb5d90fec153ae98ce33b8120ff
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-03-17 15:59:36 +0200

    Add test

---
+++ test/index.js
@@ -0,0 +1,30 @@
+const test = require('tape');
+const Animation = require('../');
+
+// Add favicon
+const linkTag = document.createElement('link');
+
+linkTag.rel = 'shortcut icon';
+linkTag.href = 'data:;base64,iVBORw0KGgo=';
+
+document.head.appendChild(linkTag);
+
+test('will throw sans callback', (t) => {
+  t.throws(Animation, Error);
+  t.end();
+});
+
+test('will avoid stacking', (t) => {
+  const animation = Animation(console.log);
+  const id = animation.start();
+
+  t.doesNotThrow(animation.start, id);
+
+  t.equals(animation.start(), id, 'frame id hasn\'t changed');
+  t.equals(animation.start(), id, 'frame id hasn\'t changed');
+  t.equals(animation.start(), id, 'frame id hasn\'t changed');
+
+  t.notOk(animation.stop());
+  t.end();
+});
+
