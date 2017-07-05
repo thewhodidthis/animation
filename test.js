a264fb1233c5f7236add0fb29edae640c124919e
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-07-05 20:51:47 +0200

    test: move one dir up

---
+++ test.js
@@ -0,0 +1,24 @@
+const kpow = require('kpow')
+const test = require('tape')
+const Animation = require('./')
+
+kpow()
+
+test('will throw sans callback', (t) => {
+  t.throws(Animation, new TypeError())
+  t.end()
+})
+
+test('will avoid stacking', (t) => {
+  const animation = Animation(Function)
+  const id = animation.start()
+
+  t.doesNotThrow(animation.start, id)
+
+  t.equals(animation.start(), id, 'frame id hasn\'t changed')
+  t.equals(animation.start(), id, 'frame id hasn\'t changed')
+  t.equals(animation.start(), id, 'frame id hasn\'t changed')
+
+  t.notOk(animation.stop(), 'stop')
+  t.end()
+})
