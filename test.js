cb41822de0d0c0dcf2ca6503b0d83e9285ab8c61
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-07-14 06:54:16 +0200

    test: cleanup

--- test.js
+++ test.js
@@ -1,7 +1,8 @@
-require('kpow')()
-
+const kpow = require('kpow')
 const test = require('tape')
 const Animation = require('./')
+
+kpow()
 
 test('will throw sans callback', (t) => {
   t.throws(Animation, new TypeError())
